183b078df567c9c920a36824f147b57237d07307
SKISHAN DORA <skisha.dora@teachingstrategies.com>
2023-07-20 16:21:33 +0530

    adding shell

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { AwsRum } from "aws-rum-web";
+
+try {
+  const config = {
+    sessionSampleRate: 1,
+    guestRoleArn: process.env.NEXT_PUBLIC_GUEST_ROLE_ARN,
+    identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
+    endpoint: process.env.NEXT_PUBLIC_ENDPOINT,
+    telemetries: ["performance", "errors", "http"],
+    allowCookies: false,
+    enableXRay: false,
+    disableAutoPageview: true,
+  };
+
+  const APPLICATION_ID = process.env.NEXT_PUBLIC_APPLICATION_ID;
+  const APPLICATION_VERSION = process.env.NEXT_PUBLIC_APPLICATION_VERSION;
+  const APPLICATION_REGION = process.env.NEXT_PUBLIC_APPLICATION_REGION;
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
